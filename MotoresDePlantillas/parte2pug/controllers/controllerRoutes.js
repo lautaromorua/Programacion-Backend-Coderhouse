bf4ecf4f262d1780cd183a6e8fb20f73ef796664
Lautaro <88165390+lautaromorua@users.noreply.github.com>
2022-06-29 20:22:03 -0300

    pug

---
+++ MotoresDePlantillas/parte2pug/controllers/controllerRoutes.js
@@ -0,0 +1,32 @@
+const productos = [];
+
+const home = (req,res) =>{
+    try {
+        res.render('home.pug')
+    } catch (error) {
+        console.log(`Error al cargar la ruta, ${error}`)
+        res.status(404)
+    };
+};
+
+const products = (req,res, next) =>{
+    res.render('productos.pug', { productos })
+};
+
+const postProducts = (req,res, next)=>{
+    productos.push(req.body)
+    res.redirect('/')
+    res.status(201).render
+};
+
+const productsAll = (req,res,next)=>{
+    try {
+        return res.send(productos);
+    } catch (error) {
+        res.status(404).res.json({ error: error.mensaje})
+    }
+};
+
+module.exports = {
+    home, products, postProducts, productsAll
+};
