21f0a283e63aa978936085aa5f22ee687a93c4d9
Lautaro <88165390+lautaromorua@users.noreply.github.com>
2022-08-04 19:39:40 -0300

    Add files via upload

---
+++ desafioMocksNormalize/controller/ReadQry.js
@@ -0,0 +1,27 @@
+const database = require('../src/database')
+
+const ReadProductos = async () => {
+    try {
+        const camposProductos = ['title', 'price', 'thumbnail']
+        const resultReadProductos = await database.databaseConnection.from('productos').select(camposProductos)
+        return resultReadProductos
+
+    } catch (err) {
+        console.log(err)
+        database.databaseConnection.destroy()
+    }
+}
+
+const ReadMensajes = async () => {
+    try {
+        const camposMensajes = ['email', 'horaenvio', 'message']
+        const resultReadMensajes = await database.databaseConnectionSQL3.from('mensajes').select(camposMensajes)
+        return resultReadMensajes
+
+    } catch (err) {
+        console.log(err)
+        database.databaseConnectionSQL3.destroy()
+    }
+}
+
+module.exports = { ReadProductos, ReadMensajes }
---
+++ desafioMocksNormalize/src/server.js
@@ -0,0 +1,51 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+const fs = require('fs')
+const { Server: IOServer } = require('socket.io')
+const expressServer = app.listen(8080, () => { console.log('Servidor conectado al puerto 8080') })
+const io = new IOServer(expressServer)
+const router = require('../routes/indexrouts.js')
+const { schema, normalize } = require('normalizr')
+const util = require('util')
+
+function print(objeto) {
+    console.log(util.inspect(objeto, false, 12, true))
+}
+
+let ProductosDB = []
+let messagesArray = []
+fs.writeFileSync(`Messages/appMensajes.txt`, '')
+app.use(express.static(path.join(__dirname, '../public')))
+app.use('/', router)
+
+io.on('connection', async socket => {
+    socket.on('client:product', async productInfo => {
+        ProductosDB = productInfo
+        io.emit('server:productos', ProductosDB)
+    })
+    socket.emit('server:productos', ProductosDB)
+
+    socket.emit('server:mensajes', messagesArray)
+    socket.on('client:menssage', async messageInfo => {
+        let MensajesExistentesFile = await fs.promises.readFile(`Messages/appMensajes.txt`)
+
+        if (MensajesExistentesFile != '') {
+            messagesArray = JSON.parse(MensajesExistentesFile)
+        }
+        messageInfo.id = messagesArray.length + 1
+        messagesArray.push(messageInfo)
+
+        await fs.promises.writeFile(`Messages/appMensajes.txt`, JSON.stringify(messagesArray))
+
+        const author = new schema.Entity('author', {}, { idAtrribute: 'id' })
+        const mensaje = new schema.Entity('mensaje', { author: author }, { idAtrribute: "id" })
+        const schemamensajes = new schema.Entity('mensajes', {
+            mensajes: [mensaje]
+        }, { idAtrribute: "id" })
+
+        const nomalizePost = normalize({ id: 'mensajes', mensajes: messagesArray }, schemamensajes)
+
+        io.emit('server:mensajes', nomalizePost)
+    })
+})
